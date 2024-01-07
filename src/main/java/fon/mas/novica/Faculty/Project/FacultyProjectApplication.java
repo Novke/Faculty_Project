da5383740adca91ff734199e6757387d88ee9482
Novke <novke.brate@gmail.com>
2024-01-07 21:37:54 +0100

    init

---
+++ src/main/java/fon/mas/novica/Faculty/Project/FacultyProjectApplication.java
@@ -0,0 +1,13 @@
+package fon.mas.novica.Faculty.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacultyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fon/mas/novica/Faculty/Project/FacultyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package fon.mas.novica.Faculty.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacultyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
