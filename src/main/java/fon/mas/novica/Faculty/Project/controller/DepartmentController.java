8b7e6b76a3ffd2336db4be8921f1cd5c9171a2fc
Novke <novke.brate@gmail.com>
2024-01-08 18:07:02 +0100

    Department CRUD

--- src/main/java/fon/mas/novica/Faculty/Project/controller/DepartmentController.java
+++ src/main/java/fon/mas/novica/Faculty/Project/controller/DepartmentController.java
@@ -1,9 +1,12 @@
 package fon.mas.novica.Faculty.Project.controller;
 
+import fon.mas.novica.Faculty.Project.entity.Department;
 import fon.mas.novica.Faculty.Project.service.DepartmentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.FileNotFoundException;
 
 @RestController
 @RequestMapping("/department")
@@ -11,5 +14,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class DepartmentController {
 
     private final DepartmentService departmentService;
+
+    @GetMapping
+    public ResponseEntity<?> getAll(){
+        return ResponseEntity.ok(departmentService.findAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    ResponseEntity<?> getDepartment(@PathVariable Long id) throws FileNotFoundException {
+        return ResponseEntity.ok(departmentService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createDepartment(@RequestBody Department department){
+        return ResponseEntity.ok(departmentService.create(department));
+    }
+
+    @PutMapping
+    public ResponseEntity<?> editDepartment(@RequestBody Department department) throws FileNotFoundException {
+        return ResponseEntity.ok(departmentService.edit(department));
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> deleteDepartment(@PathVariable Long id) throws FileNotFoundException {
+        departmentService.delete(id);
+        return ResponseEntity.ok().build();
+    }
 
 }
--- src/main/java/fon/mas/novica/Faculty/Project/service/DepartmentService.java
+++ src/main/java/fon/mas/novica/Faculty/Project/service/DepartmentService.java
@@ -1,8 +1,13 @@
 package fon.mas.novica.Faculty.Project.service;
 
+import fon.mas.novica.Faculty.Project.entity.Department;
 import fon.mas.novica.Faculty.Project.repository.DepartmentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +15,31 @@ public class DepartmentService {
 
     private final DepartmentRepository departmentRepository;
 
+    public List<Department> findAll() {
+        return departmentRepository.findAll();
+    }
+    public Department findById(Long id) throws FileNotFoundException {
+        Optional<Department> optionalDepartment = departmentRepository.findById(id);
+        if (optionalDepartment.isPresent()){
+            return optionalDepartment.get();
+        } else {
+            throw new FileNotFoundException("Department with ID = " + id + " does not exist!");
+        }
+    }
+
+    public Department create(Department department){
+        return departmentRepository.save(department);
+    }
+
+    public Department edit(Department department) throws FileNotFoundException {
+        findById(department.getId()); //baca izuzetak ako ne postoji
+
+        return departmentRepository.save(department);
+    }
+
+    public void delete(Long departmentId) throws FileNotFoundException {
+        findById(departmentId);
+
+        departmentRepository.deleteById(departmentId);
+    }
 }
