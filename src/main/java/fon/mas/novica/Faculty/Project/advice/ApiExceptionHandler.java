ca003792baa9a73127a1a51606d5327c96519dc5
Novke <novke.brate@gmail.com>
2024-01-08 18:12:47 +0100

    ApiException & FileNotFound handler

---
+++ src/main/java/fon/mas/novica/Faculty/Project/advice/ApiExceptionHandler.java
@@ -0,0 +1,20 @@
+package fon.mas.novica.Faculty.Project.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.io.FileNotFoundException;
+import java.time.ZonedDateTime;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(value = {FileNotFoundException.class})
+    public ResponseEntity<ApiException> handleFileNotFoundEx(FileNotFoundException ex){
+        ApiException apiException = new ApiException(ex.getMessage(), ZonedDateTime.now());
+        return new ResponseEntity<>(apiException, HttpStatus.NOT_FOUND);
+    }
+
+}
