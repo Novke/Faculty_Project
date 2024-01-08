31bbb53b675f4a15d6afecf621ce726a304f6ab0
Novke <novke.brate@gmail.com>
2024-01-08 18:42:02 +0100

    CRUD Subject

--- src/main/java/fon/mas/novica/Faculty/Project/controller/SubjectController.java
+++ src/main/java/fon/mas/novica/Faculty/Project/controller/SubjectController.java
@@ -1,13 +1,42 @@
 package fon.mas.novica.Faculty.Project.controller;
 
+import fon.mas.novica.Faculty.Project.entity.Subject;
 import fon.mas.novica.Faculty.Project.service.SubjectService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.FileNotFoundException;
 
 @RestController
+@RequestMapping(path = "/subject")
 @RequiredArgsConstructor
 public class SubjectController {
 
     private final SubjectService subjectService;
 
+    @GetMapping
+    ResponseEntity<?> getAll(){
+        return ResponseEntity.ok(subjectService.findAll());
+    }
+    @GetMapping(path = "/{id}")
+    ResponseEntity<?> getSubject(@PathVariable Long id) throws FileNotFoundException {
+        return ResponseEntity.ok(subjectService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createSubject(@RequestBody Subject subject){
+        return ResponseEntity.ok(subjectService.create(subject));
+    }
+
+    @PutMapping
+    public ResponseEntity<?> editSubject(@RequestBody Subject subject) throws FileNotFoundException {
+        return ResponseEntity.ok(subjectService.edit(subject));
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> deleteSubject(@PathVariable Long id) throws FileNotFoundException {
+        subjectService.delete(id);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/fon/mas/novica/Faculty/Project/service/SubjectService.java
+++ src/main/java/fon/mas/novica/Faculty/Project/service/SubjectService.java
@@ -1,12 +1,46 @@
 package fon.mas.novica.Faculty.Project.service;
 
+import fon.mas.novica.Faculty.Project.entity.Subject;
 import fon.mas.novica.Faculty.Project.repository.SubjectRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class SubjectService {
 
     private final SubjectRepository subjectRepository;
+
+    public List<Subject> findAll(){
+        return subjectRepository.findAll();
+    }
+
+    public Subject findById(Long id) throws FileNotFoundException {
+        Optional<Subject> optionalSubject = subjectRepository.findById(id);
+        if (optionalSubject.isPresent()){
+            return optionalSubject.get();
+        } else {
+            throw new FileNotFoundException("Subject with ID = " + id + " does not exist!");
+        }
+    }
+
+    public Subject create(Subject subject){
+        return subjectRepository.save(subject);
+    }
+
+    public Subject edit(Subject subject) throws FileNotFoundException {
+        findById(subject.getId());
+
+        return subjectRepository.save(subject);
+    }
+
+    public void delete(Long subjectId) throws FileNotFoundException {
+        findById(subjectId);
+
+        subjectRepository.deleteById(subjectId);
+    }
 }
