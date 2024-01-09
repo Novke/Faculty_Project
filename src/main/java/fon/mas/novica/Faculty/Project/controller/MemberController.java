90aa04f2b5c6aefc1e63c79bbf9a2005b0806d92
Novke <novke.brate@gmail.com>
2024-01-09 15:07:40 +0100

    MemberController & Service

--- src/main/java/fon/mas/novica/Faculty/Project/controller/MemberController.java
+++ src/main/java/fon/mas/novica/Faculty/Project/controller/MemberController.java
@@ -1,14 +1,57 @@
 package fon.mas.novica.Faculty.Project.controller;
 
-import fon.mas.novica.Faculty.Project.service.MemberService;
+import fon.mas.novica.Faculty.Project.entity.*;
+import fon.mas.novica.Faculty.Project.service.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.FileNotFoundException;
 
 @RestController
-@RequestMapping(path = "member")
+@RequestMapping(path = "/member")
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
+    private final DepartmentService departmentService;
+    private final AcademicTitleService academicTitleService;
+    private final ScientificFieldService scientificFieldService;
+    private final EducationTitleService educationTitleService;
+
+    @GetMapping
+    ResponseEntity<?> getAll(){
+        return ResponseEntity.ok(memberService.findAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    ResponseEntity<?> getMember(@PathVariable Long id) throws FileNotFoundException {
+        return ResponseEntity.ok(memberService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createMember(@RequestBody Member member) throws FileNotFoundException {
+        member.setDepartment(departmentService.findById(member.getDepartment().getId()));
+        member.setAcademicTitle(academicTitleService.find(member.getAcademicTitle()));
+        member.setEducationTitle(educationTitleService.find(member.getEducationTitle()));
+        member.setScientificField(scientificFieldService.find(member.getScientificField()));
+
+        return ResponseEntity.ok(memberService.create(member));
+    }
+
+    @PutMapping
+    public ResponseEntity<?> editMember(@RequestBody Member member) throws FileNotFoundException {
+        member.setDepartment(departmentService.findById(member.getDepartment().getId()));
+        member.setAcademicTitle(academicTitleService.find(member.getAcademicTitle()));
+        member.setEducationTitle(educationTitleService.find(member.getEducationTitle()));
+        member.setScientificField(scientificFieldService.find(member.getScientificField()));
+
+        return ResponseEntity.ok(memberService.edit(member));
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> deleteMember(@PathVariable Long id) throws FileNotFoundException {
+        memberService.delete(id);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/fon/mas/novica/Faculty/Project/service/MemberService.java
+++ src/main/java/fon/mas/novica/Faculty/Project/service/MemberService.java
@@ -1,9 +1,14 @@
 package fon.mas.novica.Faculty.Project.service;
 
+import fon.mas.novica.Faculty.Project.entity.Member;
 import fon.mas.novica.Faculty.Project.repository.MemberRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -11,5 +16,34 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
+    public List<Member> findAll(){
+        return memberRepository.findAll();
+    }
+
+    public Member findById(Long id) throws FileNotFoundException {
+        Optional<Member> optionalMember = memberRepository.findById(id);
+        if (optionalMember.isPresent()){
+            return optionalMember.get();
+        } else {
+            throw new FileNotFoundException("Member with ID = " + id + " does not exist!");
+        }
+    }
+
+    public Member create(Member member){
+        return memberRepository.save(member);
+    }
+
+    public Member edit(Member member) throws FileNotFoundException {
+        findById(member.getId());
+
+        return memberRepository.save(member);
+    }
+
+    public void delete(Long memberId) throws FileNotFoundException {
+        findById(memberId);
+
+        memberRepository.deleteById(memberId);
+    }
 
 }
