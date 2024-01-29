495ef6fd6bd0380aeafd7fc1c9ceaf8e59d02378
Novke <novke.brate@gmail.com>
2024-01-29 18:37:08 +0100

    Generation Strategy = IDENTITY - Member

--- src/main/java/fon/mas/novica/Faculty/Project/entity/Member.java
+++ src/main/java/fon/mas/novica/Faculty/Project/entity/Member.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private long id;
     private String firstname;
